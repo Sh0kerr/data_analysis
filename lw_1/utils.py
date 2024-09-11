ea4b0fd95d42c945c012da75ebc62176b7b397aa
Sh0kerr <vladko2003@yandex.ru>
2024-09-11 20:46:09 +0300

    fix(utils): __repr__ method & naming

--- lw_1/utils.py
+++ lw_1/utils.py
@@ -9,21 +9,24 @@ import numpy as np
 AVAILABLE_FLIP_AXIS = [0, 1]
 
 
-class Matrix:
+class Matrix2D:
     def __init__(self, vals: np.ndarray) -> None:
         self.vals = vals
 
-    def __matmul__(self, other: Matrix) -> Matrix:
-        return Matrix(self.vals @ other.vals)
+    def __repr__(self) -> str:
+        return np.array2string(self.vals)
+
+    def __matmul__(self, other: Matrix2D) -> Matrix2D:
+        return Matrix2D(self.vals @ other.vals)
     
     @classmethod
-    def from_list(cls, vals: list) -> Matrix:
+    def from_list(cls, vals: list) -> Matrix2D:
         return cls(np.array(vals))
 
-    def rotate2d(self, angle: float) -> Matrix:
-        return Matrix(self.vals @ self._2d_rotation_matrix(np.deg2rad(angle)))
+    def rotate(self, angle: float) -> Matrix2D:
+        return Matrix2D(self.vals @ self._rotation_matrix(np.deg2rad(angle)))
     
-    def flip2d(self, axis: Optional[int]=None) -> Matrix:
+    def flip(self, axis: Optional[int]=None) -> Matrix2D:
         if axis not in AVAILABLE_FLIP_AXIS:
             logging.error("Flip failed check axis value asshole")
             return self
@@ -34,21 +37,21 @@ class Matrix:
         else:
             flip_ *= -1
 
-        return Matrix(self.vals @ flip_)
+        return Matrix2D(self.vals @ flip_)
 
-    def scale(self, scaler: float) -> Matrix:
-        return Matrix(scaler * self.vals)
+    def scale(self, scaler: float) -> Matrix2D:
+        return Matrix2D(scaler * self.vals)
     
-    def inv(self) -> Matrix:
+    def inv(self) -> Matrix2D:
         try:
             _inv = np.linalg.inv(self.vals)
         except np.linalg.LinAlgError:
             logging.error("Inv failed fix your matrix hello hello hello")
             _inv = self.vals
-        return Matrix(_inv)
+        return Matrix2D(_inv)
 
     @staticmethod
-    def _2d_rotation_matrix(angle_rad: float) -> np.ndarray:
+    def _rotation_matrix(angle_rad: float) -> np.ndarray:
         return np.array(
             [
                 [np.cos(angle_rad), np.sin(angle_rad)],
@@ -57,5 +60,5 @@ class Matrix:
         )
 
 
-def plot_2d_letter(letter: Matrix) -> None:
+def plot_2d_letter(letter: Matrix2D) -> None:
     plt.plot(letter.vals[:, 0], letter.vals[:, 1])
